8ee9f69c7c855124bda2d6d07a2d72a3b40e7974
Софья <113128986+Painelka@users.noreply.github.com>
2024-03-29 19:24:52 +0200

    Update index.php

--- lab3/code/index.php
+++ lab3/code/index.php
@@ -1,1 +1,21 @@
+<!doctype html>
+<html lang ="en">
+<head>
+    <meta charset = "UTF-8">
+    <meta name="viewport"
+          content = "width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale =1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action ="save.php" method="post">
+        <label for="email">Email</label>
+        <input type ="text" name="email">
+        <select name ="category">
+            <option>doll</option>
+            <option>other</option>
+        </select>
 
+    </form>
+</body>
+</html>
