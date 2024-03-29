3f8d39b38681a47726dcdf7e2cc9184eb3de6eda
Софья <113128986+Painelka@users.noreply.github.com>
2024-03-29 19:33:53 +0200

    Update index.php

--- lab3/code/index.php
+++ lab3/code/index.php
@@ -10,12 +10,21 @@
 <body>
     <form action ="save.php" method="post">
         <label for="email">Email</label>
-        <input type ="text" name="email">
-        <select name ="category">
-            <option>doll</option>
-            <option>other</option>
+        <input type ="email" name="email" required>
+        <label for ="category">Category </label>
+        <select name ="category" required>
+            <option value="doll">doll</option>
+            <option value ="other">other</option>
         </select>
 
+        <label for="title">Title</label>
+        <input type ="text" name="title" required>
+
+        <label for="description">Description</label>
+        <textarea rows ="6" name="description"> HI! </textarea>
     </form>
+
+
+
 </body>
 </html>
