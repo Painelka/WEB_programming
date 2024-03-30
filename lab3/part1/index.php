ff1fe068994b94ce4b20ed65a101dc5f2a627cea
Софья <113128986+Painelka@users.noreply.github.com>
2024-03-30 20:40:53 +0200

    Update index.php

--- lab3/part1/index.php
+++ lab3/part1/index.php
@@ -1,3 +1,4 @@
+
 <!doctype html>
 <html lang ="en">
 
@@ -14,15 +15,30 @@
 
 </head>
 <body>
-    <?php
+<?php
 echo "<br><b>ЗАДАНИЕ 1А</b></br>";
 $str = 'ahb acb aeb aeeb adcb axeb';
 $regExp = '/a[a-z]{2}b/'; // 1- буква "а" 2 любые, а потом "b"
 $match = [];
 $count = preg_match_all($regExp, $str, $match);
-
 foreach ($match[0] as &$str)
-echo "<br> $str</br>";
+echo "<br> $str";
+echo "<br>";
+echo "<br>";
+echo "<b>ЗАДАНИЕ 1Б</b>";
+echo "<br>";
+$str2 = '2a3bc4';
+$regch = '/[0-9]/ui';
+$matcha = [];
+
+function Cube($matcha){
+    $num = (int)$matcha[0];
+    $cubnum = pow($num, 3);
+    return $cubnum;
+}
+$result = preg_replace_callback($regch, 'Cube', $str2);
+echo $result;
 ?>
+
 </body>
 </html>
