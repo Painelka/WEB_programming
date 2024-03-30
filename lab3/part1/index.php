d36b0c1988e751dcd303b8780c208414e678a2b3
Софья <113128986+Painelka@users.noreply.github.com>
2024-03-30 20:28:32 +0200

    Update index.php

--- lab3/part1/index.php
+++ lab3/part1/index.php
@@ -14,5 +14,15 @@
 
 </head>
 <body>
+    <?php
+echo "<br><b>ЗАДАНИЕ 1А</b></br>";
+$str = 'ahb acb aeb aeeb adcb axeb';
+$regExp = '/a[a-z]{2}b/'; // 1- буква "а" 2 любые, а потом "b"
+$match = [];
+$count = preg_match_all($regExp, $str, $match);
+
+foreach ($match[0] as &$str)
+echo "<br> $str</br>";
+?>
 </body>
 </html>
