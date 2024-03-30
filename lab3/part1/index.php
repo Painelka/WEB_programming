0f461b00df2cef9890191050270c7c1c0d9a4161
Софья <113128986+Painelka@users.noreply.github.com>
2024-03-30 20:26:46 +0200

    Create index.php

---
+++ lab3/part1/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang ="en">
+
+<style type="text/css">
+    thead {
+        background: #e1e1e1;
+    }
+</style>
+
+
+<head>
+    <meta charset = "UTF-8">
+
+
+</head>
+<body>
+</body>
+</html>
